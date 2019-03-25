bb62df4c8116a984c340d962dff5f246b7b1cdae
Piers Jameson <piersjameson@gmail.com>
2019-03-25 01:13:20 -0500

    First Functions and index.html

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    render_template("index.html")
+
+@app.route('/numberstuff', methods=["POST"])
+def numberStuff():
+    number = request.form.get("number")
+    print(number)
+
+if __name__ == '__main__':
+    app.run()
