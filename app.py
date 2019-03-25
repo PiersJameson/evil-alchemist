90f2b8baa63538078db92f5329a6c9c533f553a2
Piers Jameson <piersjameson@gmail.com>
2019-03-25 15:16:07 -0500

    limits added to number route, remove old test page

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from math import sqrt
 from prime import divisors
 
 app = Flask(__name__)
 
-@app.route('/')
+#Create the main route or pass POSTs onto the number facts route
+@app.route('/', methods=["GET", "POST"])
 def hello():
+    
+    # POST requests should respond with number facts
+    if request.method == "POST":
+        number = request.form["number"]
+        url = "".join(["/", str(number)])
+        return redirect(url)
+    
+    # GET requests should respond with the index page
     return render_template("index.html")
 
-@app.route('/numberstuff', methods=["POST"])
-def numberStuff():
-    number = request.form.get("number")
-    return number
 
+# Number facts route
 @app.route('/<int:number>')
 def numberReturn(number):
-    primeMessage = " is PRIME!"
+
+    # Create a warning to be passed along if submitted number is out of bounds
+    warning = ""
+    if number > 1000000 or number < 0:
+        number = 1
+        warning = "Warning! Only numbers between 1 and 1000000 please"
+    
+    # Create a list of divisors to determine primality and pass along for display
     divisorList = divisors(number)
+
+    # Create a message to pass along regarding primality
+    primeMessage = " is PRIME!"
     if len(divisorList) != 2:
         primeMessage = " is NOT prime!"
-    root = sqrt(number)
-    return render_template("number.html",number=number,divisorList=divisorList,root=root,primeMessage=primeMessage)
+    
+    #Determine the Square Root to pass along for display
+    root = "{:.4f}".format(sqrt(number))
+
+    # Render the page, passing along number, divisorList, root, warning, and primality message.
+    return render_template("number.html",number=number,divisorList=divisorList,root=root,primeMessage=primeMessage,warning=warning)
 
 if __name__ == '__main__':
     app.run()
